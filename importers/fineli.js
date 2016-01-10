3300c52ce167a343b356c5af02bd016ca3986222
Aarni Koskela <akx@iki.fi>
2016-01-10 19:13:33 +0200

    Add Fineli metadata importing

--- importers/fineli.js
+++ importers/fineli.js
@@ -48,4 +48,44 @@ function importFromDir(dir) {
   });
 }
 
+function importMetadataFromDir(dir) {
+  const keyToName = {
+    "cmpclass": "componentClass",
+    "compunit": "componentUnit",
+    "eufdname": "component",
+    "foodtype": "foodType",
+    "foodunit": "foodUnit",
+    "fuclass": "usageClass",
+    "igclass": "ingredientClass",
+    "process": "process",
+  };
+  const keysWithLanguages = ["component"].concat(R.flatten(R.map(
+    (key) => R.map(R.add(key), ["_EN", "_FI", "_SV"]),
+    Object.keys(keyToName)
+  )));
+  return util.importFilesFromDir(dir, keysWithLanguages, readFileToCsv).then((datas) => {
+    const metadata = R.fromPairs(R.map(([key, destName]) => {
+      const langMap = {};
+      Object.keys(datas).forEach((dkey) => {
+        if (dkey.indexOf(key + "_") !== 0) return;
+        datas[dkey].forEach((ent) => {
+          const id = ent.THSCODE;
+          (langMap[id] = langMap[id] || {})[ent.LANG] = ent.DESCRIPT;
+        });
+      });
+      const outMap = R.fromPairs(R.map((pair) => [pair[0], {name: pair[1]}], R.toPairs(langMap)));
+      return [destName, outMap];
+    }, R.toPairs(keyToName)));
+    datas.component.forEach((comp) => {
+      metadata.component[comp.EUFDNAME] = R.merge(metadata.component[comp.EUFDNAME], {
+        unit: comp.COMPUNIT,
+        componentClass: comp.CMPCLASS,
+        componentParentClass: comp.CMPCLASSP,
+      });
+    });
+    return metadata;
+  });
+}
+
 exports.importFromDir = importFromDir;
+exports.importMetadataFromDir = importMetadataFromDir;
